65eba32595c63c7ad3f5e0202d9454cdba36b90d
Abdallah Eid <abdallaheeid@icloud.com>
2025-11-22 15:06:17 +0100

    init project

---
+++ src/main/java/org/abdallah/githubactivity/GithubActivityApplication.java
@@ -0,0 +1,13 @@
+package org.abdallah.githubactivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActivityApplication.class, args);
+	}
+
+}
